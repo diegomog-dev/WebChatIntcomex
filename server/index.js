bce28951c5bb04ca0a43cfd5c980854364c30088
Diego Mogollon <diego.mogollon.camacho@gmail.com>
2024-08-30 09:01:53 -0500

    feat: add configuration on server

--- server/index.js
+++ server/index.js
@@ -3,8 +3,14 @@ import morgan from 'morgan';
 import http from 'http';
 import cors from 'cors';
 import realtimeServer from './realtimeServer.js'
+import path, { join } from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 
 const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const server = http.createServer(app);
 
 app.set("port", process.env.PORT || 4000);
@@ -14,6 +20,8 @@ server.listen(app.get("port"), ()=>{
 });
 
 realtimeServer(server);
+
+app.use(express.static(join(__dirname,'../client/build')))
 
 app.use(cors());
 app.use(morgan('dev'));
