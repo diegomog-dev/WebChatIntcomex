8e1fafbe6497b29abbd9ca732b9ad5723c2f1ff0
Diego Mogollon <diego.mogollon.camacho@gmail.com>
2024-08-30 08:37:25 -0500

    feat: add api

--- server/api.js
+++ server/api.js
@@ -1,5 +1,5 @@
 export default async function reqApi(parameter, count, numberPage){
-    const api = await fetch(`http://localhost:5162/api/Product/GetSearchProducts?parameter=${parameter}&count=${count}&numberPage=${numberPage}`);
+    const api = await fetch(`https://webapitestintcomex.azurewebsites.net/api/Product/GetSearchProducts?parameter=${parameter}&count=${count}&numberPage=${numberPage}`);
     const productApi = await api.json();
     return productApi;
 }
